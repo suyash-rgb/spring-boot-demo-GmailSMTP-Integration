7b420418774e2ac6d8ee30355d084c1fb3c96302
Suyash Baoney <suyashbaoney58@gmail.com>
2025-09-28 15:46:58 +0530

    project setup

---
+++ spring-boot-demo-GmailSMTP-Integration/src/main/java/com/example/spring_boot_demo_GmailSMTP_Integration/SpringBootDemoGmailSmtpIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_demo_GmailSMTP_Integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoGmailSmtpIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoGmailSmtpIntegrationApplication.class, args);
+	}
+
+}
